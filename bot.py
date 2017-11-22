d9e8025954c3c98aeef6927e2b2022db2d451b2b
Brady Kedge <bradykedge@gmail.com>
2017-11-22 17:10:29 -0600

    Basic functionality working

--- bot.py
+++ bot.py
@@ -7,3 +7,16 @@ description = 'Bot that gets gifs on command'
 
 bot = commands.Bot(command_prefix = '~', description = description)
 
+@bot.event
+async def on_ready():
+    print('Logged in')
+    print(bot.user.name)
+    print(bot.user.id)
+    print('--------')
+
+@bot.command()
+async def hello():
+    """ Says world """
+    await bot.say("world")
+
+bot.run(TOKEN)
